d8d73972acdc66fe17ae0ff0e773b56d48b4a14c
q-bit <t.quante@outlook.com>
2022-04-13 08:11:54 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, withDefaults, useSlots, useAttrs } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    msg: string;
+  }>(),
+  { msg: 'Hello World!' }
+);
+
+const emit = defineEmits<{
+  (event: 'click', count: number): void;
+}>();
+
+const slots = useSlots();
+const attributes = useAttrs()
+
+const count = ref(0);
+const increment = () => count.value++;
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button type="button" @click="increment">count is: {{ count }}</button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
